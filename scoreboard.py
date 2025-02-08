2b5098517a5aeee5f83e8af4aac9f26b0feb0fe9
souravmukherjee1301 <79442049+souravmukherjee1301@users.noreply.github.com>
2025-02-08 15:19:01 +0530

    Scoreboard Class created
    Scoreboard/Level details updated

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,28 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1  # Start at level 1
+        self.hideturtle()  # Don't show a turtle, just text
+        self.penup()  # No drawing
+        self.goto(-280, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        """Update the level displayed on the screen"""
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        """Increase level and update display"""
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        """Display 'Game Over' message."""
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
